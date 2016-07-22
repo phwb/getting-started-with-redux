9910626050666c85ac2ee9ebdb4f8e69f238ced9
Яковенко Виталий <yvv@ugraweb.ru>
2016-07-22 12:09:43 +0500

    Lesson 10: "Redux: Avoiding Object Mutations with Object.assign() and ...spread"

--- js/main.js
+++ js/main.js
@@ -1,58 +1,29 @@
-const addCounter = (list) => {
-  return [...list, 0];
+const toggleTodo = (todo) => {
+  return {
+    ...todo,
+    completed: !todo.completed
+  };
 };
 
-const removeCounter = (list, index) => {
-  return [
-    ...list.slice(0, index),
-    ...list.slice(index + 1)
-  ];
-};
+const testToggleTodo = () => {
+  const before = {
+    id: 0,
+    text: 'Learn Redux',
+    completed: false
+  };
+  const after = {
+    id: 0,
+    text: 'Learn Redux',
+    completed: true
+  };
 
-const incCounter = (list, index) => {
-  return [
-    ...list.slice(0, index),
-    list[index] + 1,
-    ...list.slice(index + 1)
-  ];
-};
-
-const testAddCounter = () => {
-  const listBefore = [];
-  const listAfter = [0];
-
-  deepFreeze(listBefore);
+  deepFreeze(before);
 
   expect(
-    addCounter(listBefore)
-  ).toEqual(listAfter);
+    toggleTodo(before)
+  ).toEqual(after);
 };
 
-const testRemoveCounter = () => {
-  const listBefore = [0, 10, 20];
-  const listAfter = [0, 20];
-
-  deepFreeze(listBefore);
-
-  expect(
-    removeCounter(listBefore, 1)
-  ).toEqual(listAfter);
-};
-
-const testIncCounter = () => {
-  const listBefore = [0, 10, 20];
-  const listAfter = [0, 11, 20];
-
-  deepFreeze(listBefore);
-
-  expect(
-    incCounter(listBefore, 1)
-  ).toEqual(listAfter);
-};
-
-
-testAddCounter();
-testRemoveCounter();
-testIncCounter();
+testToggleTodo();
 
 console.info('All tests passed.');
