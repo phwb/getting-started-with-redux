ae114584d841bc64fb994902c696fb4c5f7c6256
Яковенко Виталий <yvv@ugraweb.ru>
2016-07-22 12:11:55 +0500

    Lesson 11: "Redux: Writing a Todo List Reducer (Adding a Todo)" Lesson 12: "Redux: Writing a Todo List Reducer (Toggling a Todo)" Lesson 13: "Redux: Reducer Composition with Arrays"

--- js/main.js
+++ js/main.js
@@ -1,29 +1,103 @@
-const toggleTodo = (todo) => {
-  return {
-    ...todo,
-    completed: !todo.completed
-  };
+// 13
+const todo = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+        id: action.id,
+        text: action.text,
+        completed: false
+      };
+    case 'TOGGLE_TODO':
+      if (state.id !== action.id) {
+        return state;
+      }
+
+      return {
+        ...state,
+        completed: !state.completed
+      };
+    default:
+      return state;
+  }
 };
 
-const testToggleTodo = () => {
-  const before = {
+const todos = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        todo(undefined, action)
+      ];
+    case 'TOGGLE_TODO':
+      return state.map(t => todo(t, action));
+    default:
+      return state;
+  }
+};
+// 11
+const testAddTodos = () => {
+  const stateBefore = [];
+  const action = {
+    type: 'ADD_TODO',
     id: 0,
-    text: 'Learn Redux',
-    completed: false
+    text: 'Learn Redux'
   };
-  const after = {
-    id: 0,
-    text: 'Learn Redux',
-    completed: true
-  };
+  const stateAfter = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false
+    }
+  ];
 
-  deepFreeze(before);
+  deepFreeze(stateBefore);
+  deepFreeze(action);
 
   expect(
-    toggleTodo(before)
-  ).toEqual(after);
+    todos(stateBefore, action)
+  ).toEqual(stateAfter);
 };
 
+// 12
+const testToggleTodo = () => {
+  const stateBefore = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false
+    },
+    {
+      id: 1,
+      text: 'Go shopping',
+      completed: false
+    }
+  ];
+  const action = {
+    type: 'TOGGLE_TODO',
+    id: 1,
+  };
+  const stateAfter = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false
+    },
+    {
+      id: 1,
+      text: 'Go shopping',
+      completed: true
+    }
+  ];
+
+  deepFreeze(stateBefore);
+  deepFreeze(action);
+
+  expect(
+    todos(stateBefore, action)
+  ).toEqual(stateAfter);
+};
+
+testAddTodos();
 testToggleTodo();
 
 console.info('All tests passed.');
