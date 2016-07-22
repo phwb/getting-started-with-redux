8dd901537b92a76fc2e2128da0305acc32054e1e
Яковенко Виталий <yvv@ugraweb.ru>
2016-07-22 12:29:19 +0500

    Lesson 16: "Redux: Implementing combineReducers() from Scratch"

--- js/main.js
+++ js/main.js
@@ -46,7 +46,21 @@ const visibilityFilter = (
   }
 };
 
-const { combineReducers } = Redux;
+const combineReducers = (reducers) => {
+  return (state = {}, action) => {
+    return Object.keys(reducers).reduce(
+      (nextState, key) => {
+        nextState[key] = reducers[key](
+          state[key],
+          action
+        );
+        return nextState;
+      },
+      {}
+    );
+  };
+};
+
 const todoApp = combineReducers({
   todos,
   visibilityFilter
